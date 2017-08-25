65c3f52fac2d8126961cc7ff58bd79caa3fb05e8
Blake Abel <blakecabel@gmail.com>
2017-08-24 19:24:52 -0500

    Added method documentation to everything (Motor.java is the only thing docless)

--- hardware/MotorPair.java
+++ hardware/MotorPair.java
@@ -59,26 +59,53 @@ public class MotorPair {
         motor2.setPower(0);
     }
 
+    /**
+     * Sets the front motor (motor1) to the parameter
+     * @param power the power to set the front motor to (from -1 to 1)
+     */
     public void setFrontPower(double power){
         motor1.setPower(power);
     }
+    /**
+     * Sets the back motor (motor2) to the parameter
+     * @param power the power to set the back motor to (from -1 to 1)
+     */
     public void setBackPower(double power){
         motor2.setPower(power);
     }
 
+    /**
+     * Gets the front motor (motor1)'s current power
+     * @return the power the front motor is set to
+     */
     public double getFrontPower() { return motor1.getPower(); }
+    /**
+     * Gets the back motor (motor2)'s current power
+     * @return the power the back motor is set to
+     */
     public double getBackPower() { return motor2.getPower(); }
 
+    /**
+     * Sets the directions of both motors in the MotorPair
+     * @param direction The direction to set the motors to
+     */
     public void setDirections(DcMotorSimple.Direction direction){
         motor1.setDirection(direction);
         motor2.setDirection(direction);
     }
 
+    /**
+     * Sets the modes (RunModes) of both motors in the MotorPair
+     * @param runMode
+     */
     public void setModes(DcMotor.RunMode runMode){
         motor1.setMode(runMode);
         motor2.setMode(runMode);
     }
 
+    /**
+     * Resets the encoder values for both motors in the pair, and sets them to RUN_TO_POSITION after
+     */
     public void resetEncoders(){
         motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -86,26 +113,50 @@ public class MotorPair {
         motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
+    /**
+     * Returns the front motor (motor1)
+     * @return the front motor
+     */
     public DcMotor getMotor1() {
         return motor1;
     }
 
+    /**
+     * Returns the back motor (motor2)
+     * @return the back motor
+     */
     public DcMotor getMotor2() {
         return motor2;
     }
 
+    /**
+     * Sets the front motor (motor1) to the parameter
+     * @param motor1 the DcMotor to set the front motor to
+     */
     public void setMotor1(DcMotor motor1) {
         this.motor1 = motor1;
     }
 
+    /**
+     * sets the back motor (motor 2) to the parameter
+     * @param motor2 the DcMotor to set the back motor to
+     */
     public void setMotor2(DcMotor motor2) {
         this.motor2 = motor2;
     }
 
+    /**
+     * Returns the current average encoder value for the motors
+     * @return the average encoder value between both motors in the pair
+     */
     public int getCurrentPositionAverage(){
         return (motor1.getCurrentPosition() + motor2.getCurrentPosition()) / 2;
     }
 
+    /**
+     * Returns the current average targer encoder value
+     * @return the current average target encoder
+     */
     public int getTargetPositionAverage(){
         return (motor1.getTargetPosition() + motor2.getTargetPosition()) / 2;
     }
