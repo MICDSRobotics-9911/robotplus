7026ee719f907e17d721042c132fcbd277d438ce
Blake Abel <blakecabel@gmail.com>
2017-08-29 14:40:15 -0500

    Added javadoc for inputtracking

--- inputtracking/Input.java
+++ inputtracking/Input.java
@@ -3,46 +3,90 @@ package org.firstinspires.ftc.teamcode.robotplus.inputtracking;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 /**
- * Created by BAbel on 4/12/2017.
+ * Object class for managing writing inputs (the gamepad state every time it updates to a json file.
+ * This way, you can write an autonomous program that reads those inputs back, simulating being driven
+ * by a human for great precision and speed during autonomous.
+ * @since 4/12/17
+ * @author Blake Abel, Alex Migala
  */
-
 public class Input {
 
+    /**
+     * the current time (meant to be received from an elapsed time
+     */
     private double currentTime;
 
+    /**
+     * the gamepad's value for the left stick's Y position
+     */
     private double leftStickY;
+    /**
+     * the gamepad's value for the right stick's Y position
+     */
     private double rightStickY;
 
+    /**
+     * Empty constructor
+     */
     public Input(){
 
     }
 
+    /**
+     * Makes an input from the gamepad state (time is used to keep reading consistent during playback)
+     * @param gamepadState The gamepad object, the function will copy all of the data.
+     * @param currentTime {@link Input#currentTime}
+     */
     public Input(Gamepad gamepadState, double currentTime){
         leftStickY = gamepadState.left_stick_y;
         rightStickY = gamepadState.right_stick_y;
         this.currentTime = currentTime;
     }
 
+    /**
+     * Returns {@link Input#currentTime}
+     * @return {@link Input#currentTime}
+     */
     public double getCurrentTime() {
         return currentTime;
     }
 
+    /**
+     * Returns {@link Input#leftStickY}
+     * @return {@link Input#leftStickY}
+     */
     public double getLeftStickY() {
         return leftStickY;
     }
 
+    /**
+     * Returns {@link Input#rightStickY}
+     * @return {@link Input#rightStickY}
+     */
     public double getRightStickY() {
         return rightStickY;
     }
 
+    /**
+     * Sets {@link Input#currentTime} to the parameter
+     * @param currentTime {@link Input#currentTime}
+     */
     public void setCurrentTime(double currentTime) {
         this.currentTime = currentTime;
     }
 
+    /**
+     * Sets {@link Input#leftStickY} to the parameter
+     * @param leftStickY {@link Input#leftStickY}
+     */
     public void setLeftStickY(double leftStickY) {
         this.leftStickY = leftStickY;
     }
 
+    /**
+     * Sets {@link Input#rightStickY} to the parameter
+     * @param rightStickY {@link Input#rightStickY}
+     */
     public void setRightStickY(double rightStickY) {
         this.rightStickY = rightStickY;
     }
--- inputtracking/InputWriter.java
+++ inputtracking/InputWriter.java
@@ -9,15 +9,27 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 /**
- * Created by BAbel on 4/12/2017.
+ * Class that handles writing an array of {@link Input}s to a json file for use with {@link InputReader}
+ * It's only meant to be called at the end of the OpMode, since writing as it is running could cause too much
+ * delays or cause other performance issues with the phone
+ * @since 4/12/17
+ * @author Blake Abel, Alex Migala
  */
-
 public class InputWriter {
 
+    /**
+     * Empty constructor
+     */
     public InputWriter(){
 
     }
 
+    /**
+     * Writes the complete json file given the {@link Input} ArrayList source
+     * @param out output file to write to
+     * @param inputs the ArrayList of {@link Input}s to write
+     * @throws IOException if the output file can't be written to
+     */
     public void writeJson(OutputStream out, ArrayList<Input> inputs) throws IOException{
         JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
         writer.setIndent("    ");
@@ -25,6 +37,12 @@ public class InputWriter {
         writer.close();
     }
 
+    /**
+     * Writes the array to the json file and handles iterating through the entire ArrayList
+     * @param writer the JsonWriter writing to the output file
+     * @param inputs the ArrayList of {@link Input}s to write
+     * @throws IOException if the output file can't be written to
+     */
     public void writeInputArray(JsonWriter writer, ArrayList<Input> inputs) throws IOException{
         writer.beginArray();
         for (Input input : inputs){
@@ -33,6 +51,12 @@ public class InputWriter {
         writer.endArray();
     }
 
+    /**
+     * Writes one {@link Input} to the json file
+     * @param writer the JsonWriter writing to the output file
+     * @param input one {@link Input} from the ArrayList
+     * @throws IOException if the output file can't be written to
+     */
     public void writeInput(JsonWriter writer, Input input) throws IOException{
 
         writer.beginObject();
