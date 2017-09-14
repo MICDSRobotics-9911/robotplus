588de48b4cd943ac21aead3be3ca05bf4c77b233
Migala <amigala@micds.org>
2017-09-14 18:41:06 -0500

    ColorSensor Wrapper Class

--- autonomous/TrackerLoader.java
+++ autonomous/TrackerLoader.java
@@ -1,32 +1,56 @@
-package org.firstinspires.ftc.teamcode.robotplus.autonomoushelper;
-
-import com.vuforia.Tracker;
-import com.vuforia.VuMarkTargetResult;
-import com.vuforia.Vuforia;
+package org.firstinspires.ftc.teamcode.robotplus.autonomous;
 
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
+/**
+ * Loads the targets
+ */
 public class TrackerLoader {
+
+    /**
+     * Vuforia Instance
+     */
     private VuforiaLocalizer vuforia;
 
+    /**
+     * Array of loaded trackables
+     */
     private VuforiaTrackables trackables;
 
+    /**
+     * RelicTemplate
+     */
     private VuforiaTrackable relicTemplate;
 
+    /**
+     * Main constructor
+     * @param v vuforia instance
+     * @param asset the name of asset to load from
+     */
     public TrackerLoader(VuforiaLocalizer v, String asset) {
         this.vuforia = v;
         this.trackables = this.vuforia.loadTrackablesFromAsset(asset);
 
         this.relicTemplate = this.trackables.get(0);
-        this.relicTemplate.setName("RelicVuMark");
+        this.relicTemplate.setName(asset);
     }
 
+    /**
+     * Gets the RelicTemplate
+     * @see VuforiaTrackable
+     * @return the RelicTemplate
+     */
     public VuforiaTrackable getRelicTemplate() {
         return this.relicTemplate;
     }
 
+    /**
+     * Gets loaded targets
+     * @see VuforiaTrackables
+     * @return the trackables
+     */
     public VuforiaTrackables getTrackables() {
         return this.trackables;
     }
