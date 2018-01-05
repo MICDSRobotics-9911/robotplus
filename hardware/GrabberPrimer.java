ad49776bc50246ccfa111da9a278bd107f7b846e
stlgolfer <stlgolfer@users.noreply.github.com>
2018-01-04 18:01:05 -0600

    GrabberPrimer.java

---
+++ hardware/GrabberPrimer.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.robotplus.hardware;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by amigala on 1/4/2018.
+ */
+
+public class GrabberPrimer {
+    private Servo grabber;
+
+    public GrabberPrimer(Servo grab) {
+        this.grabber = grab;
+    }
+
+    public void initArmSystem() {
+        this.grabber.setPosition(1.0);
+        this.grabber.setPosition(0.8);
+        this.grabber.setPosition(1.0);
+    }
+
+    public void grab() {
+        this.grabber.setPosition(0);
+    }
+}
